61742ba869ff0dbba999a40ee41975c4e01d18bc
Faizan Ayubi <indianayubi@gmail.com>
2016-01-23 19:22:27 +0530

    Files Updated

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,66 @@
+// sandbox disable popups
+if (window.self !== window.top && window.name != "view1") {;
+    window.alert = function() { /*disable alert*/ };
+    window.confirm = function() { /*disable confirm*/ };
+    window.prompt = function() { /*disable prompt*/ };
+    window.open = function() { /*disable open*/ };
+}
+
+// prevent href=# click jump
+document.addEventListener("DOMContentLoaded", function() {
+    var links = document.getElementsByTagName("A");
+    for (var i = 0; i < links.length; i++) {
+        if (links[i].href.indexOf('#') != -1) {
+            links[i].addEventListener("click", function(e) {
+                console.debug("prevent href=# click");
+                if (this.hash) {
+                    if (this.hash == "#") {
+                        e.preventDefault();
+                        return false;
+                    } else {
+                        /*
+                        var el = document.getElementById(this.hash.replace(/#/, ""));
+                        if (el) {
+                          el.scrollIntoView(true);
+                        }
+                        */
+                    }
+                }
+                return false;
+            })
+        }
+    }
+}, false);
+
+(function($) {
+    "use strict";
+
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 60
+    });
+
+    new WOW().init();
+
+    $('a.page-scroll').bind('click', function(event) {
+        var $ele = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($ele.attr('href')).offset().top - 60)
+        }, 1450, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    $('#collapsingNavbar li a').click(function() {
+        /* always close responsive nav after click */
+        $('.navbar-toggler:visible').click();
+    });
+
+    $('#galleryModal').on('show.bs.modal', function(e) {
+        $('#galleryImage').attr("src", $(e.relatedTarget).data("src"));
+    });
+
+})(jQuery);
+
 $(document).ready(function() {
-	//js
+    //js
 });
